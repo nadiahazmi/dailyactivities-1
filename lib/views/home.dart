5a53f1af3fd211a848a6d76f92455a4b903ea8bb
boi-git <adhwa.aizat23@gmail.com>
2022-02-04 19:10:28 +0800

    last patch

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -10,11 +10,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Future<Album> futureAlbum;
-  String x = "xxx21312312321xx";
+  String activity_list = "";
+  List<String> activities = <String>[];
 
   void changeData() {
     setState(() {
-      x = "zzz";
       futureAlbum = fetchAlbum();
     });
   }
@@ -22,29 +22,79 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    futureAlbum = fetchAlbum();
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(
-      children: [
-        TextButton(onPressed: changeData, child: Text('ok')),
-        FutureBuilder<Album>(
-          future: futureAlbum,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Text(snapshot.data.activity);
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
+      child: Column(
+        children: [
+          TextButton(onPressed: changeData, child: Text('New Activities')),
+          FutureBuilder<Album>(
+            future: futureAlbum,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                activity_list = snapshot.data.activity;
+                return Text(snapshot.data.activity);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
 
-            // By default, show a loading spinner.
-            return const CircularProgressIndicator();
-          },
-        ),
-        Text("$x")
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+          TextButton(
+              onPressed: () {
+                showAlertDialog(context);
+              },
+              child: Text('Add to the list')),
+          Expanded(
+            child: ListView.builder(
+                itemCount: activities.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 50,
+                    margin: EdgeInsets.all(2),
+                    child: Center(
+                      child: Text('${activities[index]}'),
+                    ),
+                  );
+                }),
+          )
+        ],
+      ),
+    );
+  }
+
+  showAlertDialog(BuildContext context) {
+    // set up the button
+    Widget okButton = TextButton(
+      child: Text("Save"),
+      onPressed: () {
+        Navigator.of(context).pop();
+        setState(() {
+          activities.insert(0, activity_list);
+        });
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Daily Activities "),
+      content: Text("$activity_list"),
+      actions: [
+        okButton,
       ],
-    ));
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
   }
 }
